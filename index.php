f9c92ebee0b491f0891cdf00979eb0289cd73996
arijit.dutta <arijit.dutta@srijan.net>
2017-07-25 20:10:02 +1000

    changed directory structure

--- index.php
+++ index.php
@@ -14,21 +14,26 @@ if (isset($argv[1])) {
 	$botname = $argv[1];
 }
 
+// Define project root.
+define('INDEX_ROOT', __DIR__);
+
 // Check if the bot exists.
 $bots = glob(INDEX_ROOT . '/bots/*' , GLOB_ONLYDIR);
-if (!in_array($botname, $bots)) {
-	echo "No bot with name " . $botname . " found.";
+$botnames = array();
+foreach ($bots as $key => $bot) {
+	$botnames[] = basename($bot);
+}
+if (!in_array($botname, $botnames)) {
+	echo "No bot with name " . $botname . " found." . PHP_EOL;
 	return;
 }
 
-// Define project root.
-define('INDEX_ROOT', __DIR__);
 // Name the bot to activate.
 define('BOT_NAME', $botname);
 
 // Include files.
 include_once(INDEX_ROOT.'/vendor/autoload.php');
-include_once(INDEX_ROOT.'/core/components/irc/irc.connect.php');
+include_once(INDEX_ROOT.'/core/irc/irc.connect.php');
 
 try
 {
